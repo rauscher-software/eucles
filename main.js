acd3a3450af444e9e105956bee9e225998a208f1
row-she <row-she@gmx.de>
2024-10-20 23:11:02 +0200

    add necklace options for volume & sample, add speed options

--- main.js
+++ main.js
@@ -20,48 +20,112 @@ let started = false;
 let play = true;
 let focus = true;
 
-window.addEventListener("focus", () => focus=true);
-window.addEventListener("blur", () => focus=false);
+window.addEventListener("focus", () => focus = true);
+window.addEventListener("blur", () => focus = false);
 
 // [processing-p5-convert] import processing.sound.*;
-let bjorklund;
 let instruments;
 let interval = 150;
+let bpm = 100;
 let lastTime = 0;
 let step = 0;
 let totalSteps = 16;
 let rotateLeft = false;
 let rotateRight = false;
+let bpmCol = 0;
 
 function setup() {
   const canvas = createCanvas(640, 520);
   noStroke();
-  canvas.elt.addEventListener("contextmenu", (e) => {e.preventDefault()})
+  textSize(22);
+  textFont('monospace');
+  textAlign(CENTER, CENTER);
+  canvas.elt.addEventListener("contextmenu", (e) => { e.preventDefault() })
   instruments = new ArrayList();
   instruments.add(
     new Instrument(
-      this, "kick.wav", totalSteps, int(random(1, 9)), 9, 
+      this, "kick.wav", 0.5, 1, totalSteps, int(random(1, 9)), 9,
       0, 135, 81, 160, 120, 100
     )
   );
   instruments.add(
     new Instrument(
-      this, "snare.wav", totalSteps, int(random(1, 5)), 5,
+      this, "snare.wav", 0.5, 2, totalSteps, int(random(1, 5)), 5,
       200, 119, 168, 480, 120, 100
     )
   );
   instruments.add(
     new Instrument(
-      this, "bell.wav", totalSteps, int(random(1, 11)), 11,
+      this, "bell.wav", 0.5, 3, totalSteps, int(random(1, 11)), 11,
       131, 118, 156, 160, 400, 100
     )
   );
   instruments.add(
     new Instrument(
-      this, "stix.wav", totalSteps, int(random(1, 17)),
+      this, "stix.wav", 0.5, 4, totalSteps, int(random(1, 17)),
       17, 49, 63, 103, 480, 400, 100
     )
   );
+}
+
+function makeOptions(instrument){
+  play = false;
+  // overlay
+  let overlay = document.getElementById('options-overlay');
+  overlay.style.cssText ='background: black; z-index: 10000;';
+  // options window
+  let optionsWindow = document.createElement('div');
+  optionsWindow.classList.add('options-window');
+  optionsWindow.innerHTML += `
+    <h2 style="color: rgb(${instrument.r},${instrument.g},${instrument.b});">
+      Necklace ${instrument.index} Options
+    </h2>
+    <hr>
+    <p>Volume (0 - 10)</p>
+  `;
+  // volume
+  let volume = document.createElement('input');
+  volume.type = 'number';
+  volume.min = 0;
+  volume.max = 10;
+  volume.value = instrument.vol*10;
+  volume.addEventListener("change", (e) => {
+    instrument.vol = e.target.valueAsNumber / 10;
+    instrument.sound.setVolume(instrument.vol);
+  });
+  optionsWindow.appendChild(volume);
+  // load sample
+  let hr1 = document.createElement('hr');
+  optionsWindow.appendChild(hr1);
+  let sampleText = document.createElement('p');
+  sampleText.innerText = 'Load Sample';
+  optionsWindow.appendChild(sampleText);
+  let sampleInput = document.createElement('input');
+  sampleInput.type = 'file';
+  sampleInput.accept = 'audio/*';
+  sampleInput.onchange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        instrument.sound = loadSound(e.target.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+  optionsWindow.appendChild(sampleInput);
+  // confirm button
+  let hr2 = document.createElement('hr');
+  optionsWindow.appendChild(hr2);
+  let confirm = document.createElement('button');
+  confirm.innerText = 'OK';
+  confirm.addEventListener("click", () => {
+    overlay.innerHTML = '';
+    overlay.style.cssText = '';
+    play = true;
+  });
+  optionsWindow.appendChild(confirm);
+  overlay.appendChild(optionsWindow);
 }
 
 function mouseWheel(event) {
@@ -75,31 +139,73 @@ function mouseWheel(event) {
 }
 
 function mousePressed() {
-  for (let instrument of instruments) {
-    // add pulse
-    if (mouseButton == LEFT && instrument.isActive) {
-      instrument.pulses += 1;
-      if (instrument.pulses == 0) instrument.pulses = 1;
-      if (instrument.pulses > totalSteps) instrument.pulses = totalSteps;
-      instrument.newPattern(totalSteps, instrument.pulses); // remove pulse
-    } else if (mouseButton == RIGHT && instrument.isActive) {
-      instrument.pulses -= 1;
-      if (instrument.pulses == 0) instrument.pulses = 1;
-      if (instrument.pulses > totalSteps) instrument.pulses = totalSteps;
-      instrument.newPattern(totalSteps, instrument.pulses);
+  if (keyIsPressed !== true) {
+    for (let instrument of instruments) {
+      // add pulse
+      if (mouseButton == LEFT && instrument.isActive && play) {
+        instrument.pulses += 1;
+        if (instrument.pulses == 0) instrument.pulses = 1;
+        if (instrument.pulses > totalSteps) instrument.pulses = totalSteps;
+        instrument.newPattern(totalSteps, instrument.pulses); // remove pulse
+      } else if (mouseButton == RIGHT && instrument.isActive && play) {
+        instrument.pulses -= 1;
+        if (instrument.pulses == 0) instrument.pulses = 1;
+        if (instrument.pulses > totalSteps) instrument.pulses = totalSteps;
+        instrument.newPattern(totalSteps, instrument.pulses);
+      }
+    }
+  }
+}
+
+function doubleClicked() {
+  // if shift key is held on double click
+  if (keyIsPressed === true && keyCode == 16) {
+    for (let instrument of instruments) {
+      if (instrument.isActive){
+        makeOptions(instrument);
+      }
     }
   }
 }
 
 function keyPressed() {
   // play/pause
-  if (key == 'p'){
+  if (key == 'p') {
     play = !play;
   }
   // new random pattern
-  if (key == 'r'){
+  if (key == 'r') {
     for (let instrument of instruments) {
       instrument.newPattern(totalSteps, int(random(1, instrument.maxP)));
+    }
+  }
+  // faster
+  if (key == 'f') {
+    bpm += 1;
+    interval = 60000/(bpm*4);
+    bpmCol = 255;
+  }
+  // slower
+  if (key == 's') {
+    bpm -= 1;
+    interval = 60000/(bpm*4);
+    bpmCol = 255;
+  }
+  // enter bpm
+  if (key == 'b') {
+    play = false;
+    let speed = prompt("Please enter BPM", bpm);
+    if (speed != null){
+      let parsedBPM = parseInt(speed);
+      if (parsedBPM !== NaN && parsedBPM > 0 && parsedBPM < 450) {
+        bpm = parsedBPM;
+        interval = 60000/(bpm*4);
+        bpmCol = 255;
+      }
+      play = true;
+    }
+    else {
+      play = true;
     }
   }
 }
@@ -117,9 +223,12 @@ function startSketch() {
 
 // main draw function
 function draw() {
-  if (!started || !play || !focus) {
-    return;  // do nothing until the sketch is started
+  // check if sketch has been started, is paused or is out of focus
+  if (!started || !play || !focus) {
+    return;
   }
+  // reduce bpm brightness
+  if (bpmCol > 0) bpmCol -= 7;
   // check if instrument is being hovered
   for (let instrument of instruments) {
     instrument.isActive = instrument.hovered();
@@ -130,7 +239,7 @@ function draw() {
     background(0); // draw ring
     for (let instrument of instruments) {
       instrument.drawPattern(step);
-    } 
+    }
     // play sound and increment step
     stepForward();
     lastTime = timeNow;
@@ -161,6 +270,8 @@ function stepForward() {
 
 class Instrument {
   sound;
+  vol;
+  index;
   pattern;
   totalSteps;
   pulses;
@@ -175,6 +286,8 @@ class Instrument {
   constructor(
     sketch,
     sampleFile,
+    vol,
+    index,
     totalSteps,
     pulses,
     maxP,
@@ -186,6 +299,9 @@ class Instrument {
     radius
   ) {
     this.sound = loadSound(sampleFile);
+    this.vol = vol;
+    this.sound.setVolume(vol);
+    this.index = index;
     this.totalSteps = totalSteps;
     this.pulses = pulses;
     this.maxP = maxP;
@@ -242,13 +358,15 @@ class Instrument {
         fill(this.r, this.g, this.b);
       }
       if (this.pattern[i] == 1) {
-        ellipse(x, y, 12, 12);
+        ellipse(x, y, 4 + this.vol*16, 4 + this.vol*16);
       } else {
         ellipse(x, y, 4, 4);
       }
     }
+    fill(bpmCol);
+    text(bpm + ' BPM', 320, 260);
   }
-} 
+}
 
 function generateEuclideanPattern(steps, pulses) {
   let pattern = [];
